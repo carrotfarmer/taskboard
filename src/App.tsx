aa0f8fba9fee8cf40bb2d973f2769489956012e4
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-09-30 20:01:32 +0530

    feat: finish start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Center, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { Task, TodoistApi } from "@doist/todoist-api-typescript";
+import React, { useEffect, useState } from "react";
+import { TaskCard } from "./components/TaskCard";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>();
+  const [quote, setQuote] = useState<any>();
+
+  // call this once on page load
+  useEffect(() => {
+    const token = process.env.REACT_APP_TODOIST_TOKEN as string;
+    const todoistApi = new TodoistApi(token);
+
+    todoistApi.getTasks().then((tasks: Task[]) => {
+      if (typeof tasks != "undefined" && tasks.length > 0) {
+        setTasks(tasks);
+
+        // cache tasks in local storage
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+      } else {
+        // load tasks from local storage
+        console.log("Loading tasks from local storage");
+        const cachedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        setTasks(cachedTasks);
+      }
+    });
+
+    // fetch from quotable.io/random and modify state
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* display time in a beautiful way */}
+      <Center>
+        <Heading size="3xl" pt="10">
+          {new Date().toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          })}
+        </Heading>
+      </Center>
+      <Center>
+        {/* display quote */}
+        <Heading size="md" pt="10" color="yellow.300">
+          "{quote?.content}"
+        </Heading>
+      </Center>
+      <Center pt="2">
+        <Text fontWeight="bold">- {quote?.author}</Text>
+      </Center>
+      <SimpleGrid columns={5} spacing={5} p={5} pl={10}>
+        {tasks?.map((task: Task) => (
+          <TaskCard task={task} key={task.id} />
+        ))}
+      </SimpleGrid>
     </div>
   );
 }
---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,76 @@
+import {
+  Box,
+  Center,
+  Heading,
+  Highlight,
+  HStack,
+  Tag,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Label, Task } from "@doist/todoist-api-typescript";
+import React from "react";
+
+interface TaskCardProps {
+  task: Task;
+}
+
+export const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
+  const taskColor =
+    task.priority === 1
+      ? "gray"
+      : task.priority === 2
+      ? "blue"
+      : task.priority === 3
+      ? "orange"
+      : "red";
+
+  let date;
+
+  if (task.due) {
+    date = new Date(task.due?.date as string);
+  } else {
+    date = null;
+  }
+
+  return (
+    <Box
+      p="5"
+      borderWidth={2}
+      boxShadow="xl"
+      w="14rem"
+      borderRadius="xl"
+      _hover={{
+        transform: "scale(1.05)",
+        transition: "transform 0.2s",
+        cursor: "pointer",
+      }}
+      onClick={() => {
+        window.open(task.url);
+      }}
+    >
+      <Center>
+        <Heading fontSize="xl" color={`${taskColor}.300`}>
+          {task.content}
+        </Heading>
+      </Center>
+      <Center>
+        <Box p="5">
+          <Text>{task.description}</Text>
+          <HStack spacing={5}>
+            {task.labels.map((label) => (
+              <Tag>{label}</Tag>
+            ))}
+          </HStack>
+          {task.due && (
+            <Text pt="5" fontSize="xs">
+              {/* Due: {`${date}`} */}
+              {/* convert date to long format and render */}
+              Due: {date?.toLocaleString("en-US", { dateStyle: "long" })}
+            </Text>
+          )}
+        </Box>
+      </Center>
+    </Box>
+  );
+};
