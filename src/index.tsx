528bcde15ba0c3c382bb6f048d89dc9f4e5ad87b
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-10-01 12:54:12 +0530

    fix: cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// 2. declare your configuration, these are the defaults
 const config = {
   useSystemColorMode: true,
   initialColorMode: "dark",
 };
 
-// 3. extend the theme
 const customTheme = extendTheme({ config });
 
 const root = ReactDOM.createRoot(
